0022f7a00e22cf86e2d7960bf7da2d9374371f92
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-26 23:20:48 +0900

    return false

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -9,6 +9,9 @@ class ScoresController extends Controller
     public function show($id)
     {
         $score = Score::find($id);
+        if (!$score) {
+            return false;
+        }
 
         return response()->json($score, 200, [], JSON_UNESCAPED_UNICODE);
     }
